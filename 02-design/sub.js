d85657a52594cb8f1e6c14cf8215f79927837de4
wonjiyang <wonjiyang@gmail.com>
2023-12-29 18:43:43 +0900

    add

---
+++ 02-design/sub.js
@@ -0,0 +1,25 @@
+$('.input-cart-number').on('keyup change', function(){
+    $t = $(this);
+    
+    if ($t.val().length > 3) {
+      $t.next().focus();
+    }
+    
+    var card_number = '';
+    $('.input-cart-number').each(function(){
+      card_number += $(this).val() + ' ';
+      if ($(this).val().length == 4) {
+        $(this).next().focus();
+      }
+    })
+  
+  $('#card-holder').on('keyup change', function(){
+    $t = $(this);
+    $('.credit-card-box .card-holder div').html($t.val());
+  });
+  
+  $('#card-holder').on('keyup change', function(){
+    $t = $(this);
+    $('.credit-card-box .card-holder div').html($t.val());
+  });
+  
