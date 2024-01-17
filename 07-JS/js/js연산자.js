5746ff5137f42d41e63557947a844d324696b6f3
wonjiyang <wonjiyang@gmail.com>
2024-01-17 12:00:15 +0900

    js연산자

--- 07-JS/js/js연산자.js
+++ 07-JS/js/js연산자.js
@@ -72,6 +72,67 @@ console.log('a' && 'b' && 'c');
 
 //||(or) 연산자
 //둘 중 하나라도 true일 경우 true
+// 연산자를 기준으로 가장 먼저 만나는 true값을 반한한다.
+//모두가 false일 경우 마지막 값을 반환한다.
 console.log(true || true);
 console.log(true || false);
-console.log(false || false);
+console.log(false || false); 
+console.log(false || true);
+console.log(1 || 0);
+console.log(false || 0 || {});
+console.log("" || 0 || NaN);
+
+//병합 연산자
+//|| 연산자와 비슷하다.
+//null 또는 indefined는 제외하고 반환한다.
+console.log(0 || 5);
+console.log(0 ?? 5);
+console.log(null ?? undefined);
+
+//삼항 연산자
+//조건 ? 참일 때 반환 : 거짓일 때 반환
+console.log(1 < 2 ? '참' : '거짓');
+
+//ex
+function isCat() {
+    return (
+        //조건 ? 참 : 거짓
+        name === '고양이' ? '고양이' : '고양이 아님'
+    );
+}
+console,log(isCat('고양이'));
+console,log(isCat('개'));
+
+//배열의 전개
+//..을 사용하여 배열이나 객체를 펼칠 수 있다.
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+console.log(num1.concat(num2));
+console.log([...num1, ...num2]);
+
+//객체 전개 연산자
+const dog = {
+    name  : '멍멍이',
+    phone : '010-0000-0000'
+};
+const dogInfo = {
+    ...dog,
+    age : 2
+};
+
+//함수의 전개
+//...를 사용하여 파라미터(인수)를 전개할 수 있다.
+function sum(a, b, c) {
+    console.log(a + b + c);
+}
+sum(5, 6, 7);nn 
+
+//배열을 파라미터(인수)로 전달하려면?
+const num3 = [5, 6, 7];
+const num4 = [8, 9, 10];
+sum(num3[0], num3[1], num3[2]);
+sum(num4[0], num4[1], num4[2]);
+
+//전개 연산자를 사용해 인수 전달
+sum(...num3);
+sum(...num4);
